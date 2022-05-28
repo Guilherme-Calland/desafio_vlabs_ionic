3957176d6b88b04e5b721d0a6c4d400ca41d09f4
Guilherme <gui.crb0@gmail.com>
2022-05-28 09:28:53 -0300

    lendo dados do api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private api = 'http://157.230.55.217/api/escolas';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  readData(){
+    return this.http.get(this.api);
+  }
+
+
 }
